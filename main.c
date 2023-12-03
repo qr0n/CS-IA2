099e92f1fbbac08b257b5cdc0a5b6901962cf201
qr0n <bhaveshsharama2007@gmail.com>
2023-12-03 02:42:34 -0500

    modified:   main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 FILE* userdb;
+FILE* menudb;
+
 
 void makeUser(char username[20], char password[20])
 {
-    char fp[60];
-    char fc[40];
+    char fp[60]; // file path
+    char fc[40]; // file content 
     sprintf(fp, "%s/%s.txt", "D:/projects/compsci/db/users", username);
 
     userdb = fopen(fp, "a");
@@ -13,10 +15,19 @@ void makeUser(char username[20], char password[20])
     fclose(userdb); // Close the file after writing
 }
 
+char* getMenu()
+{
+    char buffer[1024];
+    menudb = fopen("D:/projects/compsci/db/menu.txt", "r");
+    fclose(menudb);
+    return fgets(buffer, 1000, menudb);
+
+}
+
 int main()
 {
-    printf("Hello, this is the C compiler!\n");
-    makeUser("username", "password");
-
+    printf("Menu Reader");
+    // makeUser("username", "password");
+    printf("%s", getMenu());
     return 0;
 }
