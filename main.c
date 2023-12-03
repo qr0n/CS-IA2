ad81d92496f56d4b84ffe203a4fb6cc0bfd1798b
qr0n <bhaveshsharama2007@gmail.com>
2023-12-03 02:10:00 -0500

    Updated main.c

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-char userdb[21] = "D:/projects/compsci";
+FILE* userdb;
 
-FILE* userdb
-
-void makeUser(char username[20], char password[8])
+void makeUser(char username[20], char password[20])
 {
+    char fp[60];
+    char fc[40];
+    sprintf(fp, "%s/%s.txt", "D:/projects/compsci/db/users", username);
 
+    userdb = fopen(fp, "a");
+    sprintf(fc, "%s\n%s", username, password);
+    fprintf(userdb, fc);
+    fclose(userdb); // Close the file after writing
+}
+
+int main()
+{
+    printf("Hello, this is the C compiler!\n");
+    makeUser("username", "password");
+
+    return 0;
 }
